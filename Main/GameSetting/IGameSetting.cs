a85e834072ef2da96cd07d5f739862ba18ed66d3
Oleksi89 <derdef360@gmail.com>
2024-05-11 02:39:01 +0300

    game options realization added

---
+++ Main/GameSetting/IGameSetting.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Main
+{
+    public interface IGameSetting
+    {
+        void Apply(Game game);
+    }
+
+    public class SafeStart : IGameSetting
+    {
+        public void Apply(Game game)
+        {
+            // Перший клік завжди на пусту клітинку
+            game.FirstClickIsSafe = true;
+        }
+    }
+
+    public class SafeZone : IGameSetting
+    {
+        public void Apply(Game game)
+        {
+            // Клік на цифру відкриває всі невідкриті сусідні клітинки
+            game.ClickNumberOpensAdjacentCells = true;
+        }
+    }
+
+    public class Defuse : IGameSetting
+    {
+        public void Apply(Game game)
+        {
+            // Клік на міну не закінчує гру
+            game.ClickOnMineStartsDefuseCountdown = true;
+        }
+    }
+
+    public class OpenRemaining : IGameSetting
+    {
+        public void Apply(Game game)
+        {
+            // Коли всі міни позначені, можна відкрити всі залишені клітинки
+            game.AllMinesFlaggedOpensRemainingCells = true;
+        }
+    }
+
+}
