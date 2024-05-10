2ce9bcc59260dee71b2406bd8b152f5cbf903859
Oleksi89 <derdef360@gmail.com>
2024-05-11 02:08:20 +0300

    Classes 'MineCounter' and 'GameTimer' created using Observer pattern

---
+++ Main/Observers/GameTimer.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Main
+{
+    public class GameTimer : IGameObserver
+    {
+        private System.Windows.Forms.Timer timer;
+        private Label timerLabel;
+        public double TimeElapsed;
+
+        public GameTimer(Label timerLabel)
+        {
+            this.timerLabel = timerLabel;
+            timer = new System.Windows.Forms.Timer();
+            timer.Interval = 100; // 0.1 second
+            timer.Tick += Timer_Tick;
+            timerLabel.Text = "0";
+            TimeElapsed = 0;
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            TimeElapsed += timer.Interval / 1000.0;
+            timerLabel.Text = Math.Round(TimeElapsed, 0).ToString();
+
+        }
+
+        public void Update(string message)
+        {
+            if (message != "game started" && message != "game ended")
+                return;
+
+            if (message == "game started")
+            {
+                timer.Stop();
+                TimeElapsed = 0;
+                timer.Start();
+            }
+            else if (message == "game ended")
+            {
+                timer.Stop();
+
+            }
+        }
+    }
+
+}
---
+++ Main/Observers/MineCounter.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Main
+{
+    public class MineCounter : IGameObserver
+    {
+        private Label mineCounterLabel;
+
+        public MineCounter(Label mineCounterLabel)
+        {
+            this.mineCounterLabel = mineCounterLabel;
+        }
+
+        public void Update(string message)
+        {
+            if (message != "flagged")
+                return;
+
+            if (Game.Instance.GameInProgress == false && Game.Instance.Board != null)
+            {
+                int totalMines = Game.Instance.Board.TotalMines;
+                mineCounterLabel.Text = totalMines.ToString();
+            }
+            else if (Game.Instance.GameInProgress == true && Game.Instance.Board != null)
+            {
+                int flaggedCells = Game.Instance.Board.GetCells().Count(c => c.IsFlagged);
+                int totalMines = Game.Instance.Board.TotalMines;
+                mineCounterLabel.Text = (totalMines - flaggedCells).ToString();
+            }
+            else
+            {
+                mineCounterLabel.Text = "0";
+            }
+        }
+    }
+
+}
